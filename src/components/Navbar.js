6429d0effa79437341aeb3a5ae7ee09b6fc7895f
Kevin <66994542+KevinAndary@users.noreply.github.com>
2023-09-08 17:15:20 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,32 +16,21 @@ export default class Navbar extends Component {
     render() {
         return (
             <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-                <Link to="/" className="navbar-brand logo-image">
-                    <img src="img/apple.png" alt="logo" className="logo" />
-                    <span className="company-name">CellTech</span>
+               <Link to="/">
+                    <img src="img/apple.png" alt="logo" className="navbar-brand logo-image" />
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav align-items-center">
-                        <li className="nav-item ml-5">
-                            <NavLink
-                                to="/"
-                                exact
-                                className="nav-link"
-                                activeClassName="active"
-                            >
-                                Products
-                            </NavLink>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <NavLink
+                            to="/"
+                            exact
+                            className="nav-link"
+                            activeClassName="active"
+                            onClick={() => this.setActiveLink('products')}
+                        >
+                            products
+                        </NavLink>
+                    </li>
                     </li>
                     <li className="nav-item ml-5">
                         <NavLink
