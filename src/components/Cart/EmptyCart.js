4ee8b65cf0c2e5dccfa0289c96bc5e5a65c7b964
Kevin <66994542+KevinAndary@users.noreply.github.com>
2023-09-12 01:20:59 +0300

    Update EmptyCart.js

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -6,7 +6,7 @@ export default function EmptyCart() {
   return (
     <div className="container mt-5">
       <div className="row">
-        <div className="col-10 mx-auto text-center text-title text-capitalize">
+        <div className="col-12 text-center text-title text-capitalize">
           <h1>Your cart is currently empty</h1>
           <Link to="/" className="btn btn-primary mt-3 jump-animation">
             Continue Shopping
