c3603a8af1ac7a00d6162607a49c78c81433a709
Kevin <66994542+KevinAndary@users.noreply.github.com>
2023-09-12 10:29:25 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,44 +3,44 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="container my-3 py-3">
-            <h1 className="text-center">Login</h1>
-            <hr />
-            <div className="row my-4">
-                <div className="col-md-6 mx-auto">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="Email">Email address</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                id="Email"
-                                placeholder="Email"
-                                style={{ maxWidth: '100%' }} // Adjust the input width
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="Password">Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                id="Password"
-                                placeholder="Password"
-                                style={{ maxWidth: '100%' }} // Adjust the input width
-                            />
-                        </div>
-                        <div className="my-3">
-                            <p>New Here? <Link to="/signup" className="text-decoration-underline text-info">Signup</Link> </p>
-                        </div>
-                        <div className="text-center">
-                            <button className="my-2 btn btn-dark" type="submit" disabled>
-                                Login
-                            </button>
-                        </div>
-                    </form>
+        <>
+            <div className="container my-3 py-3">
+                <h1 className="text-center">Login</h1>
+                <hr />
+                <div className="row my-4">
+                    <div className="col-md-8 mx-auto">
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="Email">Email address</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="Email"
+                                    placeholder="name@example.com"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="Password">Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="Password"
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div className="my-3">
+                                <p>New Here? <Link to="/signup" className="text-decoration-underline text-info">Signup</Link> </p>
+                            </div>
+                            <div className="text-center">
+                                <button className="my-2 btn btn-dark" type="submit" disabled>
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
